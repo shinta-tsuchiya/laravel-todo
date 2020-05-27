cd7a4805f26e206d4460b8db7c446939196fb60f
shintrb <tsuchiyashinta@gmail.com>
2020-05-27 13:26:39 +0900

    seederまで

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FoldersTableSeeder extends Seeder
 {
@@ -9,8 +11,21 @@ class FoldersTableSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        //
+        // runメソッドの中にデータを挿入するコードを記述
+        // プライベート仕事旅行、3つのフォルダを作成
+
+        $titles = ['プライベート', '仕事', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                // Carbonライブラリ 現在日時を入れる
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
